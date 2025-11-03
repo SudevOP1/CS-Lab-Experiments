24b868ebd5c90eb6aad2b41551e4d54af0e33268
SudevOP1 <sudevdahitule06@gmail.com>
2025-11-03 19:28:28 +0530

    formatting code

--- Sem5_ATCD/6_lexical_analyzer/c_code.c
+++ Sem5_ATCD/6_lexical_analyzer/c_code.c
@@ -1,22 +1,24 @@
-
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n;
     printf("Enter the size of the array: ");
     scanf("%d", &n);
-    int arr[n]; 
+    int arr[n];
 
     // taking array input
     printf("Enter %d integers:\n", n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("Enter element %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
 
     // printing array
     printf("\nDisplaying array elements:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
